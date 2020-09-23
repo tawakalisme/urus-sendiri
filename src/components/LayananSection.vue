37cf1a694ce282f4b26ed99d9a2b3f4ed7a38e06
Muhamad Iqbal Tawakal <tawakalisme@gmail.com>
2020-09-23 16:44:10 +0700

    Finishing Layanan Section

--- src/components/LayananSection.vue
+++ src/components/LayananSection.vue
@@ -3,7 +3,7 @@
     <b-row>
       <b-col>
         <h2 class="text-center">Mulai Buat Dokumen Anda dengan Mudah</h2>
-        <p class="text-center">
+        <p class="text-center mb-5 px-5">
           Kami melayani kebutuhan dokumen kependudukan Anda sepanjang hari dan
           sepanjang waktu. Pastikan untuk melengkapi seluruh poin persyaratan
           dan mengisi formulir dengan benar. Hubungi layanan telepon kami untuk
@@ -13,37 +13,39 @@
     </b-row>
     <b-row class="text-center">
       <b-col>
-        <b-card title="Kartu Tanda Penduduk">
-          <b-card-text
-            >Perekaman data baru, cetak kartu, perbaikan data dan
-            kartu</b-card-text
-          >
-          <b-button variant="primary">Buat Pengajuan</b-button>
-        </b-card>
+        <div class="card-service card-ktp">
+          <img src="@/assets/ktp.svg" alt="ktp" />
+          <h4>Kartu Tanda Penduduk</h4>
+          <p>Perekaman data baru, cetak kartu, perbaikan data dan kartu</p>
+          <b-button class="btn-a">Buat Pengajuan</b-button>
+        </div>
       </b-col>
       <b-col>
-        <b-card title="Kartu Keluarga">
-          <b-card-text
-            >Cetak kartu baru, perbaikan data dan kartu, pelaporan pindah
-            datang</b-card-text
-          >
-          <b-button variant="primary">Buat Pengajuan</b-button>
-        </b-card>
-      </b-col>
+        <div class="card-service card-kk">
+          <img src="@/assets/kk.svg" alt="ktp" />
+          <h4>Kartu Keluarga</h4>
+          <p>
+            Cetak kartu baru, perbaikan data dan kartu, pelaporan pindah datang
+          </p>
+          <b-button class="btn-b">Buat Pengajuan</b-button>
+        </div></b-col
+      >
       <b-col>
-        <b-card title="Pencatatan Baru">
-          <b-card-text
-            >Akta kelahiran, akta kematian, dan perbaikan data</b-card-text
-          >
-          <b-button variant="primary">Buat Pengajuan</b-button>
-        </b-card>
-      </b-col>
+        <div class="card-service card-akta">
+          <img src="@/assets/akta.svg" alt="ktp" />
+          <h4>Pencatatan Sipil</h4>
+          <p>Akta kelahiran, akta kematian, dan perbaikan data</p>
+          <b-button class="btn-c">Buat Pengajuan</b-button>
+        </div></b-col
+      >
     </b-row>
   </b-container>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "LayananSection",
+};
 </script>
 
-<style></style>
+<style lang="scss"></style>
